c89d2b69c2fe4b2871b39ddb3f4d926175340a7c
Your Name <zaccheus.lines@hotmail.com>
2023-11-19 16:01:46 +0000

    Encoding utf-8 to ISO-8859-1

--- spam_filter.py
+++ spam_filter.py
@@ -5,17 +5,17 @@ import os
 def read_folder(folder_path):
     data = []
     for file in os.listdir(folder_path):
-        if file.endswith(".txt"):
-            file_path = os.path.join(folder_path, file)
-            with open(file_path, 'r', encoding='utf-8') as f:
-                content = f.read()
-                data.append(content)
+        #if file.endswith(".txt"): 
+        file_path = os.path.join(folder_path, file)
+        with open(file_path, 'r', encoding='ISO-8859-1') as f:
+            content = f.read()
+            data.append(content)
     return pd.DataFrame(data, columns=['email_content'])
 
 # Paths to the folders
-hard_spam_path = 'SpamAssassin Public Corpus/hard spam'
-easy_spam_path = 'SpamAssassin Public Corpus/easy spam'
-not_spam_path = 'SpamAssassin Public Corpus/not spam'
+hard_spam_path = 'SpamAssassin Public Corpus/spam_2/spam_2'
+easy_spam_path = 'SpamAssassin Public Corpus/easy_ham/easy_ham'
+not_spam_path = 'SpamAssassin Public Corpus/hard_ham/hard_ham'
 
 # Reading each folder
 hard_spam = read_folder(hard_spam_path)
@@ -30,6 +30,4 @@ not_spam['label'] = 'not_spam'
 # Combine all data into a single DataFrame
 all_emails = pd.concat([hard_spam, easy_spam, not_spam])
 
-# Your processing code goes here
-
-# Save or use the DataFrame as needed
+print(all_emails)

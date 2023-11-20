23fb5349ad0a21abb8b362566dc4ac0296b09e8b
Your Name <zaccheus.lines@hotmail.com>
2023-11-20 14:44:43 +0000

    Update spam_filter.py

--- spam_filter.py
+++ spam_filter.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import email
+from email.parser import Parser
 import os
+from bs4 import BeautifulSoup
 
 # Function to read all txt files in a folder and return a DataFrame
 def read_folder(folder_path):
@@ -12,22 +15,64 @@ def read_folder(folder_path):
             data.append(content)
     return pd.DataFrame(data, columns=['email_content'])
 
+def parse_email(email_text):
+    # Parse the email
+    msg = Parser().parsestr(email_text)
+
+    # Extract details
+    from_ = msg['Return-Path']
+    subject_ = msg['subject']
+    date_ = msg['date']
+    content_type_ = msg.get_content_type()
+    payload = msg.get_payload()
+
+    # Get the email body
+    if msg.is_multipart():
+        for part in msg.walk():
+            if part.get_content_type() == 'text/plain' or part.get_content_type() == 'text/html':
+                payload = part.get_payload(decode=True).decode()
+                break
+    else:
+        payload_ = msg.get_payload(decode=True).decode('ISO-8859-1')
+
+    # Parse HTML content to extract text if it's HTML
+    if 'html' in content_type_:
+        soup = BeautifulSoup(payload, 'lxml')
+        text_content = soup.get_text()
+    else:
+        text_content = payload
+
+    # Create a DataFrame
+    df = pd.DataFrame({
+        'From': [from_],
+        'Subject': [subject_],
+        'Date': [date_],
+        'Content': [text_content]
+    })
+
+    return df
+
 # Paths to the folders
-hard_spam_path = 'SpamAssassin Public Corpus/spam_2/spam_2'
-easy_spam_path = 'SpamAssassin Public Corpus/easy_ham/easy_ham'
-not_spam_path = 'SpamAssassin Public Corpus/hard_ham/hard_ham'
+hard_spam_path = 'SpamCorpus/spam'
+easy_spam_path = 'SpamCorpus/easy_ham'
+ham_path = 'SpamCorpus/hard_ham'
 
 # Reading each folder
 hard_spam = read_folder(hard_spam_path)
 easy_spam = read_folder(easy_spam_path)
-not_spam = read_folder(not_spam_path)
+ham = read_folder(ham_path)
 
 # Optionally, add a label column
 hard_spam['label'] = 'hard_spam'
 easy_spam['label'] = 'easy_spam'
-not_spam['label'] = 'not_spam'
+ham['label'] = 'ham'
 
 # Combine all data into a single DataFrame
-all_emails = pd.concat([hard_spam, easy_spam, not_spam])
+corpus = pd.concat([hard_spam, easy_spam, ham])
+dfs_to_concat =[]
+for index, row in corpus.iterrows():
+    dfs_to_concat.append(parse_email(row['email_content']))
 
-print(all_emails)
+concatenated_df = pd.concat(dfs_to_concat)
+
+print(concatenated_df)

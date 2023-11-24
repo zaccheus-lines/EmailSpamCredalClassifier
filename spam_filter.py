91fa3ddec88fc65ba968fde509dc7d956f360cd6
Your Name <zaccheus.lines@hotmail.com>
2023-11-24 18:20:30 +0000

    Update spam_filter.py

--- spam_filter.py
+++ spam_filter.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 import pandas as pd
 import email
 from email.parser import Parser
@@ -6,6 +7,7 @@ from bs4 import BeautifulSoup
 import time
 import re
 from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 
 start_time = time.time()
 
@@ -15,12 +17,18 @@ def safe_decode(payload, encoding='ISO-8859-1'):
     except UnicodeDecodeError:
         return payload.decode(encoding, errors='ignore')
 
-def preprocess(text):
-    # Lowercasing, removing non-alphabetic characters.
-    text = text.lower()
-    text = re.sub(r'[^a-zA-Z]', ' ', text)
-    text = re.sub(r'\s+', ' ', text).strip()
-    return text
+def tokenize(text: str) -> set[str]:
+    # Normalize text: lowercase and replace newlines with spaces
+    text = text.lower().replace('\n', ' ')
+
+    text = re.sub(r'<[^>]+>', ' ', text)
+
+    #pattern = r'\b\w+\b(?:\s+\b\w+\b)?'
+    pattern = r'\b\w+\b'
+    words = re.findall(pattern, text)
+    
+    tokens = set(words)
+    return tokens
 
 def parse_email(email_text, label):
     # Parse the email
@@ -42,23 +50,11 @@ def parse_email(email_text, label):
     else:
         payload = safe_decode(msg.get_payload(decode=True))
 
-    # Parse HTML content to extract text if it's HTML
-    if 'html' in content_type_:
-        soup = BeautifulSoup(payload, 'lxml')
-        text_content = soup.get_text()
-    else:
-        text_content = payload
-    
-    text_content= preprocess(text_content)
-
-
-
     # Create a DataFrame
     df = pd.DataFrame({
         'From': [from_],
         'Subject': [subject_],
-        'Date': [date_],
-        'Content': [text_content],
+        'Content': [payload],
         'Label': [label]
     })
 
@@ -89,32 +85,44 @@ hard_spam = read_folder(hard_ham_path)
 easy_ham = read_folder(easy_ham_path)
 hard_ham = read_folder(hard_ham_path)
 
-# Optionally, add a label column
-hard_spam['label'] = 'hard_spam'
-easy_ham['label'] = 'easy_ham'
+
+hard_spam['label'] = 'spam'
+easy_ham['label'] = 'ham'
 hard_ham['label'] = 'ham'
 
 # Combine all data into a single DataFrame
 corpus = pd.concat([hard_spam, easy_ham, hard_ham])
 
 # Apply the parse_email function to the 'email_content' column of the first 10 rows
-parsed_rows = corpus.iloc[:1000].apply(lambda row: parse_email(row['email_content'], row['label']), axis=1)
+parsed_rows = corpus.iloc[:].apply(lambda row: parse_email(row['email_content'], row['label']), axis=1)
 
 # Concatenate the results
 concatenated_df = pd.concat(parsed_rows.tolist())
 
-# Vectorize the preprocessed text
-vectorizer = CountVectorizer(analyzer='word', binary=True)
+union_set = set()
+for email in concatenated_df['Content']:
+    tokens = tokenize(email)
+    union_set.update(tokens)
 
-X = vectorizer.fit_transform(concatenated_df['Content'])
 
-# Convert to DataFrame for better readability
-binary_df = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names_out())
+# Prepare a list to store the vectors and labels
+data = []
 
-# Optionally, you can concatenate this binary_df with the original DataFrame
-final_df = pd.concat([concatenated_df.reset_index(drop=True), binary_df], axis=1)
+# Iterate over each email and its label
+for email, label in zip(concatenated_df['Content'], concatenated_df['Label']):
+    # Tokenize the email
+    email_tokens = set(tokenize(email))
 
-print(final_df)
+    # Create a binary vector for this email
+    vector = [int(token in email_tokens) for token in union_set]
+
+    # Append the vector and label as a dictionary to the data list
+    data.append({'vector': np.array(vector), 'label': label})
+
+# Create a new DataFrame from the list
+vector_df = pd.DataFrame(data)
+
+print(vector_df)
 
 elapsed = time.time() - start_time
 print(f"Time elapsed: {elapsed} seconds")
